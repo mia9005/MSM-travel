889cfcd84240805846df3b546c0e17763f84f6b5
Jeongeun Lee <jungeun9005@gmail.com>
2023-07-27 01:20:09 -0700

    change the code simply

--- js/shopping.js
+++ js/shopping.js
@@ -11,26 +11,29 @@ function contentPosition() {
     const content = tooltip.querySelector(".tooltip-content");
     const arrow = tooltip.querySelector(".arrow");
     const pinLeft = pin.offsetLeft;
-    if (pinLeft + content.offsetWidth / 2 > fullDiv.offsetWidth) {
-      const extraLeft =
-        fullDiv.offsetWidth - (pinLeft + content.offsetWidth / 2);
-      // console.log('right-conflict', tooltip)
-      content.style.left =
-        pinLeft - content.offsetWidth / 2 + extraLeft - 30 + "px";
-      content.style.top = pin.offsetTop + 30 + "px";
-    } else if (
-      pin.offsetLeft + container.offsetLeft <
-      content.offsetWidth / 2
-    ) {
-      // console.log('left conflict', pin.offsetLeft)
-      content.style.left = -container.offsetLeft + "px";
-      content.style.top = pin.offsetTop + 30 + "px";
-    } else {
-      content.style.left = pinLeft - content.offsetWidth / 2 + "px";
-      content.style.top = pin.offsetTop + 30 + "px";
-    }
-    arrow.style.left =
-      pinLeft - content.offsetLeft + pin.offsetWidth / 2 + "px";
+
+    content.style.left = pinLeft - content.offsetWidth / 2 + "px";
+    content.style.top = pin.offsetTop + 30 + "px";
+    // if (pinLeft + content.offsetWidth / 2 > container.offsetWidth) {
+    //   const extraLeft =
+    //     container.offsetWidth - (pinLeft + content.offsetWidth / 2);
+    //   // console.log('right-conflict', tooltip)
+    //   content.style.left =
+    //     pinLeft - content.offsetWidth / 2 + "px";
+    //   content.style.top = pin.offsetTop +30 + "px";
+    // } else if (
+    //   pin.offsetLeft + container.offsetLeft <
+    //   content.offsetWidth / 2
+    // ) {
+    //   // console.log('left conflict', pin.offsetLeft)
+    //   content.style.left = -container.offsetLeft + "px";
+    //   content.style.top = pin.offsetTop + 30 + "px";
+    // } else {
+    //   content.style.left = pinLeft - content.offsetWidth / 2 + "px";
+    //   content.style.top = pin.offsetTop + 30 + "px";
+    // }
+    // arrow.style.left =
+    //   pinLeft - content.offsetLeft + pin.offsetWidth / 2 + "px";
   });
 }
 tooltips.forEach((tooltip) => {
@@ -41,21 +44,19 @@ tooltips.forEach((tooltip) => {
   });
   pin.addEventListener("mouseleave", () => {
     timeoutId = setTimeout(() => {
-      if (!tooltip.classList.contains("content-hover")) {
-        tooltip.classList.remove("active");
-      }
+      tooltip.classList.remove("active");
     }, 2000);
   });
   content.addEventListener("mouseover", () => {
     clearTimeout(timeoutId);
     tooltip.classList.add("active");
-    tooltip.classList.add("content-hover");
+    
   });
   content.addEventListener("mouseleave", () => {
     timeoutId = setTimeout(() => {
       tooltip.classList.remove("active");
-      tooltip.classList.remove("content-hover");
-    }, 2000);
+      
+    }, 5000);
   });
 });
 
